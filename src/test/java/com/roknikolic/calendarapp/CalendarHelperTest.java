ba84b0948fd3f1cd8a3c2891177e35cf71ceb945
Rok Nikolič <rok.nikolic@gmail.com>
2024-08-07 17:11:17 +0200

    Simplified formula only and unit tests for new functions

--- src/test/java/com/roknikolic/calendarapp/CalendarHelperTest.java
+++ src/test/java/com/roknikolic/calendarapp/CalendarHelperTest.java
@@ -40,27 +40,66 @@ class CalendarHelperTest {
     @Test
     void numOfDaysInMonthOddFirstHalf() {
         int result = calendarHelper.numOfDaysInMonth(1, 2024);
-        assertEquals(result, 31);
+        assertEquals(31, result);
     }
     @Test
     void numOfDaysInMonthFebLeap() {
         int result = calendarHelper.numOfDaysInMonth(2, 2024);
-        assertEquals(result, 29);
+        assertEquals(29, result);
     }
     @Test
     void numOfDaysInMonthFebNormal() {
         int result = calendarHelper.numOfDaysInMonth(2, 2023);
-        assertEquals(result, 28);
+        assertEquals(28, result);
     }
     @Test
     void numOfDaysInMonthOddSecondHalf() {
         int result = calendarHelper.numOfDaysInMonth(11, 2024);
-        assertEquals(result, 30);
+        assertEquals(30, result);
     }
     @Test
     void numOfDaysInMonthEvenSecondHalf() {
         int result = calendarHelper.numOfDaysInMonth(12, 2024);
-        assertEquals(result, 31);
+        assertEquals(31, result);
     }
-
+    @Test
+    void dayOfTheWeek1() {
+        int result = calendarHelper.getWeekDayForDate(1,1, 2024);
+        assertEquals(1, result);
+    }
+    @Test
+    void dayOfTheWeek2() {
+        int result = calendarHelper.getWeekDayForDate(4,1, 2024);
+        assertEquals(4, result);
+    }
+    @Test
+    void dayOfTheWeek3() {
+        int result = calendarHelper.getWeekDayForDate(1,3, 1000);
+        assertEquals(6, result);
+    }
+    @Test
+    void dayOfTheWeek4() {
+        int result = calendarHelper.getWeekDayForDate(9,7, 1255);
+        assertEquals(5, result);
+    }
+    @Test
+    void dayOfTheWeek5() {
+        int result = calendarHelper.getWeekDayForDate(1,4, 3000);
+        assertEquals(2, result);
+    }
+    @Test
+    void dayOfTheWeek6() {
+        int result = calendarHelper.getWeekDayForDate(1,3, 300);
+        assertEquals(4, result);
+    }
+    @Test
+    void firstDayOfMonth1() {
+        int result = calendarHelper.getFirstDayOfMonth(2, 2024);
+        assertEquals(4, result);
+    }
+    @Test
+    void firstDayOfMonth2() {
+        int result = calendarHelper.getFirstDayOfMonth(6, 1344);
+        assertEquals(1, result);
+    }
 }
